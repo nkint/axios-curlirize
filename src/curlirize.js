7fac2a7eb336af5d5724354e6d8f1bc4edb40e87
alberto <alberto.massa@ymail.com>
2018-09-12 13:45:16 +0200

    in the browser too

--- src/curlirize.js
+++ src/curlirize.js
@@ -1,11 +1,16 @@
 import { CurlHelper } from './lib/CurlHelper';
 import logger from 'fancy-log';
+import * as isBrowser from 'is-browser'
 
 export default (instance) => {
     instance.interceptors.request.use(req => {
         let curl = new CurlHelper(req);
         req.curlCommand = curl.generateCommand();
-        logger.info(req.curlCommand);
+        if (!isBrowser) {
+            logger.info(req.curlCommand);
+        } else {
+            console.info(req.curlCommand)
+        }
         return req;
     });
 }
